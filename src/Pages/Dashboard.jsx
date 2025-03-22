fb27987896ecbff1bce83fdb087a9a346578f4a5
Chandan510 <chandansuthar510@gmail.com>
2025-03-22 15:30:56 +0530

    Routing

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,103 +1,146 @@
-import React from 'react'
+import React from "react";
 
 function Dashboard() {
   return (
     <>
-      <div className='w-[100%] border border-red-500 px-5 '>
-        <div className='py-4'>
-          <h1 className='text-2xl font-bold'>Dashboard</h1>
+      <div className="w-[100%] border border-red-500 px-5 ">
+        <div className="py-4">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
         </div>
 
-        <div className='flex items-center justify-center gap-3'>
-          <div className='w-[33%] border py-2 px-4 rounded-2xl'>
-            <h2 className='text-2xl text-bold'>&#x20B9; 150000</h2>
+        <div className="flex items-center justify-center gap-3">
+          <div className="w-[33%] border py-2 px-4 rounded-2xl">
+            <h2 className="text-2xl text-bold">&#x20B9; 150000</h2>
             <p>Invested</p>
           </div>
-          <div className='w-[33%] border py-2 px-4 rounded-2xl'>
-            <h2 className='text-2xl text-bold'>&#x20B9; 150000</h2>
+          <div className="w-[33%] border py-2 px-4 rounded-2xl">
+            <h2 className="text-2xl text-bold">&#x20B9; 150000</h2>
             <p>Current Value</p>
           </div>
-          <div className='w-[33%] border py-2 px-4 rounded-2xl'>
-            <h2 className='text-2xl text-bold'>&#x20B9; 150000</h2>
+          <div className="w-[33%] border py-2 px-4 rounded-2xl">
+            <h2 className="text-2xl text-bold">&#x20B9; 150000</h2>
             <p>Total P/L</p>
           </div>
         </div>
 
-        <div className='my-3 flex items-center gap-3'>
-         
-            <div className='w-[100%] flex items-center  gap-3 '>
+        <div className="my-3 flex items-center gap-3 h-[75vh] mt-8">
+          <div className="w-[100%] flex   gap-4   h-[75vh] ">
+            {/* Left */}
 
-              <div className='w-2/4 '>
-              <div className=' px-2 pb-2'>
-                <h3 className='text-md font-bold'>Chart Stocks</h3>
-                
-              </div>
-
-                <div className='px-2 pb-2'>
-                <h3 className='text-md font-bold'>Top Performing Stocks</h3>
-                <ul className='pt-1 flex flex-col gap-2'>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
+            <div className="w-2/4 flex-1  flex flex-col">
+              {/* 3 */}
+              <div className=" px-2 pb-2 border-1 rounded-xl flex-1">
+                <h3 className="text-md font-bold">Market Stocks</h3>
+                <ul className="pt-1 flex flex-col gap-2">
+                  <li className=" py-1 px-2 ">Sampe</li>
+                  <li className=" py-1 px-2 ">Sampe</li>
+                  <li className=" py-1 px-2 ">Sampe</li>
+                  <li className=" py-1 px-2 ">Sampe</li>
                 </ul>
               </div>
-
-              </div>
-
-
-
-              <div className='w-2/4'>
-
-              <div className=' px-2 pb-2'>
-                <h3 className='text-md font-bold'>Market Stocks</h3>
-                <ul className='pt-1 flex flex-col gap-2'>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
+              {/* 4 */}
+              <div className=" px-2 pb-2 border-1 rounded-xl p-4 mt-5 flex-1">
+                <h3 className="text-md font-bold">Low Performing Stocks</h3>
+                <ul className="pt-1 flex flex-col gap-2">
+                  <li className="">Sampe</li>
+                  <li className="">Sampe</li>
+                  <li className="">Sampe</li>
+                  <li className="">Sampe</li>
                 </ul>
               </div>
-              <div className=' px-2 pb-2'>
-                <h3 className='text-md font-bold'>Low Performing Stocks</h3>
-                <ul className='pt-1 flex flex-col gap-2'>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
-                <li className='border py-1 px-2 rounded-lg'>Sampe</li>
+            </div>
+            {/* Right */}
+
+            <div className="w-2/4 flex-1 flex flex-col">
+              {/* 3 */}
+              <div className=" px-2 pb-2 border-1 rounded-xl flex-1">
+                <h3 className="text-md font-bold">Market Stocks</h3>
+                <ul className="pt-1 flex flex-col gap-2">
+                  <li className=" py-1 px-2 ">Sampe</li>
+                  <li className=" py-1 px-2 ">Sampe</li>
+                  <li className=" py-1 px-2 ">Sampe</li>
+                  <li className=" py-1 px-2 ">Sampe</li>
                 </ul>
               </div>
-
-
+              {/* 4 */}
+              <div className=" px-2 pb-2 border-1 rounded-xl p-4 mt-5 flex-1">
+                <h3 className="text-md font-bold">Low Performing Stocks</h3>
+                <ul className="pt-1 flex flex-col gap-2">
+                  <li className="">Sampe</li>
+                  <li className="">Sampe</li>
+                  <li className="">Sampe</li>
+                  <li className="">Sampe</li>
+                </ul>
               </div>
-            </div>
-
-          <div className='mt-3 w-[400px] h-[68vh] py-1 px-3 text-xl border rounded-xl flex flex-col justify-between'>
-            <div  className='text-center'>
-              <h2>Ai Assistant</h2>
-            </div>
-
-            <div>
-              <p>message</p>
-            </div>
-
-            <div className='flex items-center gap-3'>
-              <input type="text" name="inp" id="inp" className='border rounded-lg'/>
-              <button>
-              <i class="fa-solid fa-check"></i>
-              </button>
             </div>
           </div>
 
+          <div className="w-[400px] h-[75vh] py-1 px-3 text-xl border rounded-xl flex flex-col justify-between">
+            <div className="text-center">
+              <h2>Ai Assistant</h2>
+            </div>
 
+            <div className="overflow-y-auto py-5 ps-1 no-scrollbar text-start px-5">
+              <p className="text-[16px]">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Tempore facere dolor quam, ab eligendi minus nulla officia
+                blanditiis eius veniam nemo voluptatibus? Repellat saepe totam
+                laudantium, dolorum pariatur beatae cum vel magni impedit
+                explicabo nulla natus consectetur odio voluptatibus fugit
+                voluptates debitis in nesciunt soluta, voluptas incidunt
+                adipisci molestiae reiciendis facere? Laboriosam modi enim iste,
+                in soluta voluptatem eveniet esse corporis quibusdam molestiae
+                aliquid repellendus unde nobis illo corrupti accusantium eaque
+                rerum. Sunt eum necessitatibus eius nobis cupiditate, velit
+                repellat ea rem culpa doloribus tenetur id vitae facilis aperiam
+                aspernatur impedit consequatur, consectetur amet! Incidunt
+                exercitationem expedita, sit provident adipisci tempora
+                doloribus magni, blanditiis reiciendis placeat ratione facere
+                ipsa architecto! Earum incidunt dolore sapiente delectus sed
+                neque harum vero fugiat numquam. Praesentium sapiente nemo odit
+                ad quisquam eius quod illum pariatur recusandae nostrum
+                explicabo deleniti dolorum iste facilis nulla nisi aut
+                reiciendis, possimus est distinctio molestias esse amet! Odit
+                enim quod consectetur fugiat animi. Placeat tempore sint,
+                reprehenderit corporis sapiente asperiores fuga quibusdam ab,
+                enim distinctio nostrum eum voluptatem in? Nesciunt ducimus
+                veniam sed incidunt labore id eum consectetur? Vel voluptate
+                minus labore, accusamus dolore incidunt quae debitis dicta!
+                Voluptates asperiores amet perferendis tenetur? Perspiciatis
+                repellendus iste eos dolorem, in eaque quam pariatur et suscipit
+                repudiandae excepturi, explicabo ipsum corporis natus impedit
+                saepe, ullam sunt perferendis corrupti quisquam nobis neque
+                consequatur. Sint eligendi rem pariatur explicabo numquam, optio
+                reprehenderit error ratione dicta deleniti minus temporibus
+                magni quos repudiandae, aperiam quo iure ea incidunt repellat
+                neque officiis esse aspernatur est doloremque. Maxime, aliquam
+                corporis autem repudiandae eveniet expedita esse fuga repellat
+                cupiditate repellendus. Temporibus, dolor explicabo culpa magni
+                cumque quia qui eligendi commodi sunt saepe! Mollitia, totam id
+                labore dignissimos architecto voluptatibus consequuntur maxime
+                repellat exercitationem voluptate eveniet quod blanditiis
+                quibusdam, nobis itaque ut molestias neque optio illo, esse
+                libero cum.
+              </p>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <input
+                type="text"
+                name="inp"
+                id="inp"
+                className="border rounded-lg w-full h-[30px] my-2 text-sm px-3 font-semibold"
+                placeholder="Ask AI"
+              />
+              <button>
+                <i class="fa-solid fa-check"></i>
+              </button>
+            </div>
+          </div>
         </div>
-
-
-
       </div>
-
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/Pages/Sidebar.jsx
+++ src/Pages/Sidebar.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
+import React from "react";
+import { NavLink, useNavigate } from "react-router";
 
 function Sidebar() {
-    return (
-        <div>
-            <div className='w-[200px] h-lvh bg-[#032313] text-white flex flex-col gap-5 ' >
+  const navigate = useNavigate();
 
-                <div className='py-5 flex gap-2.5 items-center justify-center'>
-                    <i className="fa-solid fa-chart-pie text-2xl"></i>
-                    <h1 className='text-xl'>Invester.com</h1>
-                </div>
-
-                <div>
-                    <div className='py-1 px-7 flex gap-2.5 items-center'>
-                        <i className="fa-solid fa-list text-xl"></i>
-                        <h1 className='text-lg'>Dashbord</h1>
-                    </div>
-
-                </div>
-
-
-            </div>
-
+  return (
+    <div>
+      <div className="w-[200px] h-lvh bg-[#032313] text-white flex flex-col gap-5 ">
+        <div className="py-5 flex gap-2.5 items-center justify-center">
+          <i className="fa-solid fa-chart-pie text-2xl"></i>
+          <h1 className="text-xl">Invester.com</h1>
         </div>
-    )
+
+        <div>
+          <div
+            onClick={() => navigate("/")}
+            className="py-1 px-7 flex gap-2.5 items-center cursor-pointer"
+          >
+            <i className="fa-solid fa-list text-xl"></i>
+            <h1 className="text-lg">Dashbord</h1>
+          </div>
+          <div
+            onClick={() => navigate("/stocks")}
+            className="py-1 px-7 flex gap-2.5 items-center cursor-pointer"
+          >
+            <i className="fa-solid fa-list text-xl"></i>
+            <h1 className="text-lg">Stocks</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
